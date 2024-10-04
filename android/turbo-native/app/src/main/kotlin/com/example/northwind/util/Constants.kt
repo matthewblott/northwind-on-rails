48638df6cd45110106591d45c0c6b5f2266c947d
Matthew Blott <m4ttb@pm.me>
2024-10-04 23:20:13 +0100

    saved

--- android/turbo-native/app/src/main/kotlin/com/example/northwind/util/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.northwind.util
-
-const val BASE_URL = "http://10.0.2.2:3000"
-const val HOME_URL = BASE_URL
-const val LOGOUT_URL = "$BASE_URL/turbo-logout"
-const val LOGIN_URL = "$BASE_URL/employees/sign_in"
